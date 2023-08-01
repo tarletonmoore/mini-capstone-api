d6be373193a5a19877009308c51bf666c8ba2ab7
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-01 15:22:16 -0400

    add update and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,20 @@ class ProductsController < ApplicationController
                               description: params[:input_description])
     render template: "products/show"
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render template: "products/show"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "You done deleted the product" }
+  end
 end
