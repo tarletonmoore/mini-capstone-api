b63b1a57eb2967f12dc679a3f0c85625f0f70bb3
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-01 13:39:21 -0400

    product test index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
 end
