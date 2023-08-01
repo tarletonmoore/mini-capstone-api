ce527728c4816c194e7774e76da4687f9d7ed560
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-01 14:06:10 -0400

    create route hardcoded

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,9 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
+
+  def create
+    @product = Product.create(name: "Mario Kart 64", price: 40, image_url: "https://upload.wikimedia.org/wikipedia/en/a/a1/Mario_Kart_64.jpg", description: "Mario Kart 64 is a kart racing video game developed and published by Nintendo for the Nintendo 64.")
+    render template: "products/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products" => "products#index"
-  # get "/first" => "products#first_product"
   get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
