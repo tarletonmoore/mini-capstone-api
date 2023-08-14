0e18ab38d541e79c9c94ba9503d7659180eff670
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-14 13:42:47 -0400

    add cartedproducts model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,13 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 10..500 }
   belongs_to :supplier
-  has_many :orders
+  # has_many :orders
   has_many :images
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :orders, through: :carted_products
+  has_many :users, through: :carted_products
 
   # def supplier
   #   Supplier.find_by(id: id)
